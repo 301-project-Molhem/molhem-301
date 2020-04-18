2280a38dc5cf24f265798716dab5255380d92f67
Your Name In Single Quotes <your email here in single quotes>
2020-04-18 19:09:46 +0300

    main page pair programming with fatima

--- server.js
+++ server.js
@@ -20,12 +20,40 @@ app.set('view engine', 'ejs');
 app.use(cors());
 
 
-app.get('/', (request, response) => {
-    response.status(200).send('Home Page');
-});
 
-app.use('*', notFoundHandler);
 
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+app.get('/', topten);
+function topten(req, res) {
+let key=process.env.PIXABAY_API_KEY;
+let pixUrl=`https://pixabay.com/api/?key=${key}&q=design`;
+console.log(key);
+    superagent(pixUrl)
+        .then(pixRes => {
+           console.log(pixRes);
+            let pixData = pixRes.body.hits.map((element) => {
+                return new Photo(element)
+            })
+           
+            res.render('pages/index', { photos: pixData })
+            
+        }).catch((err) => {
+            errorHandler(err, req, res);
+        });
+}
+///////////////////constructor/////////////////////////////////////////////////////
+function Photo(data) {
+    this.title = data.tags;
+    this.creator_name = data.user;
+    this.categories = data.type;
+    this.source_URL = data.pageURL;
+    this.likes = data.likes;
+    this.img_url = data.largeImageURL;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 client.connect().then(() => {
     app.listen(PORT, () => console.log('up and running in port', PORT));
@@ -33,6 +61,7 @@ client.connect().then(() => {
 
 
 // handler functions
+app.use('*', notFoundHandler);
 function notFoundHandler(request, response) {
     response.status(404).send('Not Found');
 }
