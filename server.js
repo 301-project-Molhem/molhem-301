24be4c2f51438896f2a257c188a8b2c798d145fa
Your Name In Single Quotes <your email here in single quotes>
2020-04-18 18:46:51 +0300

    edit on constrcutor in server.js and edit on div of pixabayResult

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (request, response) => {
     response.status(200).send('Home Page');
 });
 
-app.get('/search',(request, response)=>{
+app.get('/search', (request, response) => {
     response.render('/pages/search');
 });
 
@@ -34,36 +34,46 @@ app.post('/search', (req, res) => {
     let searchKeyword = req.body.picName;
     let apiSelect = req.body.selectApi;
     imageUrlPix = `https://pixabay.com/api/?key=16102900-d7963a65628f8edaa82e9259f&q=${searchKeyword}`;
-    videoUrlPix = `https://pixabay.com/api/videos/?key=16102900-d7963a65628f8edaa82e9259f&q=${searchKeyword}`; 
+    videoUrlPix = `https://pixabay.com/api/videos/?key=16102900-d7963a65628f8edaa82e9259f&q=${searchKeyword}`;
     // imageUrlGoogle = `https://pixabay.com/api/?key=16102900-d7963a65628f8edaa82e9259f&q=${searchKeyword}`; //change
 
     if (apiSelect === 'Pixabay') {
-        if (searchType === 'image'){
+        if (searchType === 'image') {
 
-        }else if (searchType === 'video') {
+        } else if (searchType === 'video') {
 
         }
- 
+
     } else if (apiSelect === 'google') {
 
     }
 
+    let ideaCollection = [];
+
+    superagent.get(url)
+        .then(pic => {
+            let arrayItems = pic.body.hits;
+            let photos = arrayItems.map(data => {
+                let photoItem = new Photo(data);
+                ideaCollection.push(photoItem);
+                return photoItem;
+            })
+            response.render('pages/search', { keyPhoto: photos });
+        })
+        .catch(error => {
+            res.render('pages/error');
+        });
+});
 
 
-superagent.get(url)
-.then(data=>{
-    let arrayItems=data.body.hits;
-
-})
 
 function Photo(data) {
-    this.title = hits.tags;
-    this.creator_name=hits.user;
-    this.categories=hits.type;
-    this.source_URL=hits.pageURL;
-    this.image_url=hits.largeImageURL;
-    this.likes=hits.likes;
-    this.owner=hits.user;
+    this.title = data.hits.tags;
+    this.creator_name = data.hits.user;
+    this.categories = data.hits.type;
+    this.source_URL = data.hits.pageURL;
+    this.image_url = data.hits.largeImageURL;
+    this.likes = data.hits.likes;
 }
 
 
